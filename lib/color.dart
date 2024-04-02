866bf38edd65dbb61b1c0e44800a4ed74ca1e542
Om Bharsakle <ombharsakle418@gmail.com>
2024-04-02 21:46:33 +0530

    02-04-24

--- lib/color.dart
+++ lib/color.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xFF240A34);
 Color groyColor = const Color(0xFF891652);
-Color onst CosecondaryColor = clor(0xFFEABE6C);
+Color  CosecondaryColor = Color(0xFFEABE6C);
 Color bgColor = const Color(0xFFFFEDD8);
